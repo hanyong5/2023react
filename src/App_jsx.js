9a2876e0137ab68141a0acbc0958299448c5d999
hanyong5 <hanyong5@naver.com>
2023-11-10 15:39:15 +0900

    component

---
+++ src/App_jsx.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+
+
+function App() {
+  let data = "안녕하세요" 
+  return (
+    <div className="App">
+     {data}
+    </div>
+  );
+}
+
+export default App;
